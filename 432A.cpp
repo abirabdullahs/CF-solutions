59dc6715217b8b4981abe77c236ef8dcbe4fbdb8
Abir Abdullah <abirabdullah3491@gmail.com>
2025-09-22 12:48:32 +0600

    432

---
+++ 432A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int n,k;
+    cin>>n>>k;
+    int a[n],b[n];
+    int c=0;
+
+    for (int i=0; i<n; i++){
+        cin>>a[i];
+        b[i] = 5 - a[i];
+        if(b[i]>=k) c++;
+    }
+
+    cout<<c/3<<endl;
+}
